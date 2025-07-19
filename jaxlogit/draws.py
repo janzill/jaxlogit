bfd1a15df7cd7c023925e629b16f3848e3051e9f
Jan Zill <jzill@anl.gov>
2025-07-18 20:03:29 -0500

    adds robert sequence, grad logging for memory alloc tests

--- jaxlogit/draws.py
+++ jaxlogit/draws.py
@@ -7,6 +7,79 @@ import numpy as np
 
 logger = logging.getLogger(__name__)
 
+## currently a pr at https://github.com/jax-ml/jax/pull/23808/files
+def _newton_raphson(f, x, iters):
+    """Use the Newton-Raphson method to find a root of the given function."""
+
+    def update(x, _):
+        y = x - f(x) / jax.grad(f)(x)
+        return y, None
+
+    x, _ = jax.lax.scan(update, x, length=iters)
+    return x
+
+
+def roberts_sequence(
+    num: int,
+    dim: int,
+    root_iters: int = 10_000,
+    complement_basis: bool = True,
+    key=None,  ## ArrayLike | None = None,
+    perturb: bool = False,
+    shuffle: bool = False,
+    dtype=float,  # DTypeLikeFloat = float,
+):
+    """Returns the Roberts sequence, a low-discrepancy quasi-random sequence:
+    Low-discrepancy sequences are useful for quasi-Monte Carlo methods.
+    Reference:
+    Martin Roberts. The Unreasonable Effectiveness of Quasirandom Sequences.
+    extremelearning.com.au/unreasonable-effectiveness-of-quasirandom-sequences
+    Args:
+      num: Number of points to return.
+      dim: The dimensionality of each point in the sequence.
+      root_iters: Number of iterations to use to find the root.
+      complement_basis: Complement the basis to improve precision, as described
+        in https://www.martysmods.com/a-better-r2-sequence.
+      key: a PRNG key.
+      perturb: Apply a uniformly random perturbation to the entire sequence,
+        followed by modulo 1.
+      shuffle: Shuffle the elements of the sequence before returning them.
+        Warning: This degrades the low-discrepancy property for prefixes of
+        the output sequence.
+      dtype: optional, a float dtype for the returned values (default float64 if
+        jax_enable_x64 is true, otherwise float32).
+    Returns:
+      An array of shape (num, dim) containing the sequence.
+    """
+
+    def f(x):
+        return x ** (dim + 1) - x - 1
+
+    root = _newton_raphson(f, jnp.astype(1.0, dtype), root_iters)
+
+    basis = 1 / root ** (1 + jnp.arange(dim, dtype=dtype))
+
+    if complement_basis:
+        basis = 1 - basis
+
+    n = jnp.arange(num, dtype=dtype)
+    x = n[:, None] * basis[None, :]
+
+    if perturb:
+        if key is None:
+            raise ValueError("key for roberts_sequence cannot be None when perturb=True")
+        key, subkey = jax.random.split(key)
+        x += jax.random.uniform(subkey, (dim,), dtype)[None]
+
+    x, _ = jnp.modf(x)
+
+    if shuffle:
+        if key is None:
+            raise ValueError("key for roberts_sequence cannot be None when shuffle=True")
+        x = jax.random.permutation(key, x)
+
+    return x
+
 
 # TODO: have a look at scipy.stats.qmc, has sobol draws
 def generate_draws(sample_size, n_draws, _rvdist, halton=True, halton_opts=None):
